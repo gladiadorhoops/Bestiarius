4d91deafcd3e30eaccd0c97653edce957cb41a2b
Jesus Gardea <jegardea@amazon.com>
2022-04-17 23:40:08 -0700

    a

---
+++ gladiadores-hoops/src/app/app.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+
+  constructor(private http: HttpClient) { }
+  rootURL = '/';
+
+  getRoot() {
+    return this.http.get(this.rootURL);
+  }
+}
