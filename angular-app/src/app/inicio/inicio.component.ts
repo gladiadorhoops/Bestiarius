4d39b21d967c15cdc295fcc59405431812d017b5
Daniele Braga Pecanha <daniele.pecanha@gmail.com>
2022-07-20 18:21:52 -0700

    Add results for each day

--- angular-app/src/app/inicio/inicio.component.ts
+++ angular-app/src/app/inicio/inicio.component.ts
@@ -12,23 +12,49 @@ const S3_BUCKET_URL = (day: number) => `https://gladiadores-hoops.s3.amazonaws.c
 export class InicioComponent implements OnInit {
 
   matches: Match[] = [];
+  days: number[] = [29, 30, 31];
+  matchesDays: Match[][] = [];
   showResults: boolean = false;
   todayDay = new Date().getDate()
 
   constructor(private httpService: HttpClient) { }
   ngOnInit(): void {
     
-    this.httpService.get(S3_BUCKET_URL(this.todayDay)).subscribe(
+    this.loadMatchesPerDay(this.days[0]);
+    this.loadMatchesPerDay(this.days[1]);
+    this.loadMatchesPerDay(this.days[2]);
+  }
+
+  loadMatchesPerDay(day: number) {
+    let display_date = this.todayDay;
+    if(this.days[0] > this.todayDay) {
+      display_date = this.days[0];
+    }
+
+    this.httpService.get(S3_BUCKET_URL(day)).subscribe(
       (response) => {
         console.log('response received', response);
-        this.matches = (response as any).matches as Match[];
-        this.showResults = this.matches.length > 0;
-        console.log("showResults", this.showResults)
+        this.matchesDays[day] = (response as any).matches as Match[];
+        
+        if(day == display_date)
+        {
+          this.matches = this.matchesDays[day];
+          this.showResults = this.matches.length > 0;
+          console.log("showResults", this.showResults)
+        }
       },
       (error) => {
         console.error(error)
-        this.showResults = false
+        if(day == display_date)
+        {
+          this.showResults = false;
+        }
       }
     )
+  }
+
+  showDay(day: number) {
+    this.matches = this.matchesDays[day];
+    this.showResults = this.matches.length > 0;
   }
 }
