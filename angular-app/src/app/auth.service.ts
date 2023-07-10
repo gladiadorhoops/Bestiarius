93f33a75a65ca2de42314ad476bb9e5f9d2a8c6b
Daniele Braga Pecanha <daniele.pecanha@gmail.com>
2023-07-10 00:32:10 -0700

    remove unused code

--- angular-app/src/app/auth.service.ts
+++ angular-app/src/app/auth.service.ts
@@ -3,8 +3,6 @@ import { Injectable } from '@angular/core';
 import { Cognito } from './aws-clients/cognito';
 import { DynamoDb } from './aws-clients/dynamodb';
 
-const LOGIN_URL =  'https://gladiadores-hoops.s3.amazonaws.com/match-data/tournament-10/scoutid.json'
-//const LOGIN_URL =  'https://gladiadores-hoops.s3.amazonaws.com/match-data/tournament-10/category-matches-2022-07-29.json'
 
 @Injectable({
   providedIn: 'root'
@@ -23,7 +21,7 @@ export class AuthService {
        if(credentials == undefined){
          return
        }
-       
+
        console.log("credentials are valid")
        ddb = new DynamoDb(credentials)
 
@@ -65,63 +63,3 @@ export class AuthService {
       }
   } 
 }
-
-
-
-
-/*import {Request, Response} from "express";
-import express from 'express';
-//const bodyParser = require('body-parser');
-//const cookieParser = require('cookie-parser');
-import * as jwt from 'jsonwebtoken';
-import * as fs from "fs";
-
-const app: express.Application = express();
-
-//app.use(bodyParser.json());
-
-app.route('/api/login')
-    .post(loginRoute);
-
-const RSA_PRIVATE_KEY = fs.readFileSync('./demos/private.key');
-
-export function loginRoute(req: Request, res: Response) {
-
-    const scout = req.body.email,
-          password = req.body.password;
-
-    if (validateEmailAndPassword()) {
-       const userId = findUserIdForEmail(scout);
-
-        const jwtBearerToken = jwt.sign({}, RSA_PRIVATE_KEY, {
-                algorithm: 'RS256',
-                expiresIn: 120,
-                subject: userId
-            }
-        );
-
-        // send the JWT back to the user
-        // TODO - multiple options available                              
-        
-        // set it in an HTTP Only + Secure Cookie
-        res.cookie("SESSIONID", jwtBearerToken, {httpOnly:true, secure:true});
-
-    }
-    else {
-        // send status 401 Unauthorized
-        res.sendStatus(401); 
-    }
-}
-
-function validateEmailAndPassword(){
-    
-    return true
-}
-
-function findUserIdForEmail(scout: any) {
-    
-    
-    return scout
-}
-
-*/
