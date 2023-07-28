56e35ffcd7a2b034baa00cad91bc344286150dad
Daniele Braga Pecanha <daniele.pecanha@gmail.com>
2023-07-28 16:53:30 -0700

    Add filter view for resultados

--- angular-app/src/app/resultados-evaluacion/resultados-evaluacion.component.ts
+++ angular-app/src/app/resultados-evaluacion/resultados-evaluacion.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-resultados-evaluacion',
@@ -7,6 +8,9 @@ import { Component } from '@angular/core';
 })
 export class ResultadosEvaluacionComponent {
 
+  constructor(private fb: FormBuilder){
+    
+  }
   top5s = [
     {
       award:"Tiro", 
@@ -265,23 +269,35 @@ export class ResultadosEvaluacionComponent {
     }
   ]
 
+  equipos = [{"name":"Equipo A"}, {"name":"Equipo B"}, {"name":"Equipo C"}, {"name":"Equipo D"}]
+
   players = [
-    {Name: "Daniel", Equipo: "Equipo A", Scouts: 10, Position: [{posicion: "1", votes: 4}, {posicion: "2", votes: 0}, {posicion: "3", votes: 0}, {posicion: "4", votes: 7}, {posicion: "5", votes: 0}], Evalgen: "4.5", Style: [{style: "Anotador", votes: 4}, {style: "Defensor", votes: 0}, {style: "Creador", votes: 0}, {style: "Atletico", votes: 0}, {style: "Clutch", votes: 0}, {style: "Rebotador", votes: 1}, {style: "Rol", votes: 8}]},
-    {Name: "Luis", Equipo: "Equipo B", Scouts: 8, Position: [{posicion: "1", votes: 0}, {posicion: "2", votes: 0}, {posicion: "3", votes: 8}, {posicion: "4", votes: 0}, {posicion: "5", votes: 0}], Evalgen: "4.2", Style: [{style: "Anotador", votes: 0}, {style: "Defensor", votes: 6}, {style: "Creador", votes: 0}, {style: "Atletico", votes: 0}, {style: "Clutch", votes: 3}, {style: "Rebotador", votes: 0}, {style: "Rol", votes: 0}]},
-    {Name: "Pedro", Equipo: "Equipo C", Scouts: 5, Position: [{posicion: "1", votes: 0}, {posicion: "2", votes: 0}, {posicion: "3", votes: 0}, {posicion: "4", votes: 4}, {posicion: "5", votes: 0}], Evalgen: "4.8", Style: [{style: "Anotador", votes: 0}, {style: "Defensor", votes: 0}, {style: "Creador", votes: 4}, {style: "Atletico", votes: 0}, {style: "Clutch", votes: 0}, {style: "Rebotador", votes: 0}, {style: "Rol", votes: 2}]},
-    {Name: "Juan", Equipo: "Equipo D", Scouts: 9, Position: [{posicion: "1", votes: 0}, {posicion: "2", votes: 6}, {posicion: "3", votes: 0}, {posicion: "4", votes: 0}, {posicion: "5", votes: 3}], Evalgen: "3.9", Style: [{style: "Anotador", votes: 0}, {style: "Defensor", votes: 0}, {style: "Creador", votes: 0}, {style: "Atletico", votes: 7}, {style: "Clutch", votes: 0}, {style: "Rebotador", votes: 4}, {style: "Rol", votes: 0}]},
-    {Name: "Jose", Equipo: "Equipo A", Scouts: 7, Position: [{posicion: "1", votes: 0}, {posicion: "2", votes: 0}, {posicion: "3", votes: 2}, {posicion: "4", votes: 1}, {posicion: "5", votes: 0}], Evalgen: "4.7", Style: [{style: "Anotador", votes: 0}, {style: "Defensor", votes: 0}, {style: "Creador", votes: 0}, {style: "Atletico", votes: 0}, {style: "Clutch", votes: 1}, {style: "Rebotador", votes: 0}, {style: "Rol", votes: 0}]}
+    {Name: "Daniel", Equipo: "Equipo A", Scouts: 10, Position: [{posicion: "1", votes: 4}, {posicion: "2", votes: 0}, {posicion: "3", votes: 0}, {posicion: "4", votes: 7}, {posicion: "5", votes: 0}], Evalgen: "4.5", Style: [{style: "Anotador", votes: 4}, {style: "Defensor", votes: 0}, {style: "Creador", votes: 0}, {style: "Atletico", votes: 0}, {style: "Clutch", votes: 0}, {style: "Rebotador", votes: 1}, {style: "Rol", votes: 8}],skills: [{Name: "Tiro", Points: 3},{Name: "Pase", Points: 0},{Name: "Defensa", Points: 0},{Name: "Bote", Points: 0},{Name: "Jugador", Points: 0}], nominations: [{Name: "Maximus", Points: 0}]},
+    {Name: "Luis", Equipo: "Equipo B", Scouts: 8, Position: [{posicion: "1", votes: 0}, {posicion: "2", votes: 0}, {posicion: "3", votes: 8}, {posicion: "4", votes: 0}, {posicion: "5", votes: 0}], Evalgen: "4.2", Style: [{style: "Anotador", votes: 0}, {style: "Defensor", votes: 6}, {style: "Creador", votes: 0}, {style: "Atletico", votes: 0}, {style: "Clutch", votes: 3}, {style: "Rebotador", votes: 0}, {style: "Rol", votes: 0}],skills: [{Name: "Tiro", Points: 0},{Name: "Pase", Points: 0},{Name: "Defensa", Points: 9},{Name: "Bote", Points: 0},{Name: "Jugador", Points: 0}], nominations: [{Name: "Maximus", Points: 2}]},
+    {Name: "Pedro", Equipo: "Equipo C", Scouts: 5, Position: [{posicion: "1", votes: 0}, {posicion: "2", votes: 0}, {posicion: "3", votes: 0}, {posicion: "4", votes: 4}, {posicion: "5", votes: 0}], Evalgen: "4.8", Style: [{style: "Anotador", votes: 0}, {style: "Defensor", votes: 0}, {style: "Creador", votes: 4}, {style: "Atletico", votes: 0}, {style: "Clutch", votes: 0}, {style: "Rebotador", votes: 0}, {style: "Rol", votes: 2}],skills: [{Name: "Tiro", Points: 0},{Name: "Pase", Points: 0},{Name: "Defensa", Points: 0},{Name: "Bote", Points: 0},{Name: "Jugador", Points: 0}], nominations: [{Name: "Maximus", Points: 0}]},
+    {Name: "Juan", Equipo: "Equipo D", Scouts: 9, Position: [{posicion: "1", votes: 0}, {posicion: "2", votes: 6}, {posicion: "3", votes: 0}, {posicion: "4", votes: 0}, {posicion: "5", votes: 3}], Evalgen: "3.9", Style: [{style: "Anotador", votes: 0}, {style: "Defensor", votes: 0}, {style: "Creador", votes: 0}, {style: "Atletico", votes: 7}, {style: "Clutch", votes: 0}, {style: "Rebotador", votes: 4}, {style: "Rol", votes: 0}],skills: [{Name: "Tiro", Points: 0},{Name: "Pase", Points: 0},{Name: "Defensa", Points: 0},{Name: "Bote", Points: 0},{Name: "Jugador", Points: 0}], nominations: [{Name: "Maximus", Points: 4}]},
+    {Name: "Jose", Equipo: "Equipo A", Scouts: 7, Position: [{posicion: "1", votes: 0}, {posicion: "2", votes: 0}, {posicion: "3", votes: 2}, {posicion: "4", votes: 1}, {posicion: "5", votes: 0}], Evalgen: "4.7", Style: [{style: "Anotador", votes: 0}, {style: "Defensor", votes: 0}, {style: "Creador", votes: 0}, {style: "Atletico", votes: 0}, {style: "Clutch", votes: 1}, {style: "Rebotador", votes: 0}, {style: "Rol", votes: 0}],skills: [{Name: "Tiro", Points: 0},{Name: "Pase", Points: 0},{Name: "Defensa", Points: 0},{Name: "Bote", Points: 0},{Name: "Jugador", Points: 0}], nominations: [{Name: "Maximus", Points: 0}]}
   ]
 
-  selectedPlayer = {Name: "", Equipo: "", Scouts: 0, Position: [{posicion: "1", votes: 0}, {posicion: "2", votes: 0}, {posicion: "3", votes: 0}, {posicion: "4", votes: 0}, {posicion: "5", votes: 0}], Evalgen: "0", Style: [{style: "Anotador", votes: 0}, {style: "Defensor", votes: 0}, {style: "Creador", votes: 0}, {style: "Atletico", votes: 0}, {style: "Clutch", votes: 0}, {style: "Rebotador", votes: 0}, {style: "Rol", votes: 0}]}
+  selectedPlayer = {Name: "", Equipo: "", Scouts: 0, Position: [{posicion: "1", votes: 0}, {posicion: "2", votes: 0}, {posicion: "3", votes: 0}, {posicion: "4", votes: 0}, {posicion: "5", votes: 0}], Evalgen: "0", Style: [{style: "Anotador", votes: 0}, {style: "Defensor", votes: 0}, {style: "Creador", votes: 0}, {style: "Atletico", votes: 0}, {style: "Clutch", votes: 0}, {style: "Rebotador", votes: 0}, {style: "Rol", votes: 0}],skills: [{Name: "Tiro", Points: 0},{Name: "Pase", Points: 0},{Name: "Defensa", Points: 0},{Name: "Bote", Points: 0},{Name: "Jugador", Points: 0}], nominations: [{Name: "Maximus", Points: 0}]}
     
+  filterForm = this.fb.group({
+    cat: null,
+    player: null,
+    equipo: null
+  });
+
   updateSelected(playerName: string){
-    var localselectedplayer= {Name: "", Equipo: "", Scouts: 0, Position: [{posicion: "1", votes: 0}, {posicion: "2", votes: 0}, {posicion: "3", votes: 0}, {posicion: "4", votes: 0}, {posicion: "5", votes: 0}], Evalgen: "0", Style: [{style: "Anotador", votes: 0}, {style: "Defensor", votes: 0}, {style: "Creador", votes: 0}, {style: "Atletico", votes: 0}, {style: "Clutch", votes: 0}, {style: "Rebotador", votes: 0}, {style: "Rol", votes: 0}]}
+    var localselectedplayer= {Name: "", Equipo: "", Scouts: 0, Position: [{posicion: "1", votes: 0}, {posicion: "2", votes: 0}, {posicion: "3", votes: 0}, {posicion: "4", votes: 0}, {posicion: "5", votes: 0}], Evalgen: "0", Style: [{style: "Anotador", votes: 0}, {style: "Defensor", votes: 0}, {style: "Creador", votes: 0}, {style: "Atletico", votes: 0}, {style: "Clutch", votes: 0}, {style: "Rebotador", votes: 0}, {style: "Rol", votes: 0}],skills: [{Name: "Tiro", Points: 0},{Name: "Pase", Points: 0},{Name: "Defensa", Points: 0},{Name: "Bote", Points: 0},{Name: "Jugador", Points: 0}], nominations: [{Name: "Maximus", Points: 0}]}
     this.players.forEach(function(value){
       if(value.Name == playerName){
         localselectedplayer = value;
       }
     });
     this.selectedPlayer = localselectedplayer;
+  }
+
+  applyFilters(){
+    // TODO: filter teams and players based on category/team selection
   }
 }
