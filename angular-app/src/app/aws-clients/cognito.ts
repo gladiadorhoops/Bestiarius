37bfc9042c0a51c3c9c956b7ce302721d2253fac
Jesus Gardea <jgardea35@outlook.com>
2024-04-28 23:57:26 -0700

    Cognito: Add User Signup & retrieving user data from sign in
    * Added Cognito calls to sign up and send verification code
    * Now all the user attributes are passed in from the OIDC ID token
    * Updated identity and user pool values. Now all users comes from the same identity and user pool, but permissions are separated by the role claim

--- angular-app/src/app/aws-clients/cognito.ts
+++ angular-app/src/app/aws-clients/cognito.ts
@@ -1,54 +1,75 @@
-import { CognitoIdentityProviderClient, InitiateAuthCommand, InitiateAuthCommandInput, InitiateAuthCommandOutput } from "@aws-sdk/client-cognito-identity-provider";
+import { 
+    CognitoIdentityProviderClient, 
+    InitiateAuthCommand, 
+    InitiateAuthCommandInput, 
+    InitiateAuthCommandOutput,
+    SignUpCommandInput,
+    SignUpCommandOutput,
+    SignUpCommand,
+    ConfirmSignUpCommandInput,
+    ConfirmSignUpCommandOutput,
+    ConfirmSignUpCommand,
+    ResendConfirmationCodeCommand,
+    ResendConfirmationCodeCommandInput,
+    ResendConfirmationCodeCommandOutput
+} from "@aws-sdk/client-cognito-identity-provider";
 import { AwsCredentialIdentity, Provider } from "@aws-sdk/types"
-import { REGION, SCOUT_APP_CLIENT } from "./constants";
 import { CognitoIdentityCredentialProvider, fromCognitoIdentityPool } from "@aws-sdk/credential-providers";
-import { COGNITO_IDENTITY_POOL, COGNITO_IDP_TEMPLATE, COGNITO_USER_POOL } from "./constants";
+import { 
+    REGION, 
+    SCOUT_APP_CLIENT,
+    GLADIADORES_APP_CLIENT_ID,
+    COGNITO_IDENTITY_POOL, 
+    COGNITO_IDP_TEMPLATE, 
+    COGNITO_USER_POOL,
+} from "./constants";
+import { User } from "../interfaces/user";
+import { jwtDecode } from "jwt-decode";
+
 
 const client = new CognitoIdentityProviderClient({region: REGION})
 
 export class Cognito {
 
-    static async getAwsCredentials(username: string, password: string): Promise<AwsCredentialIdentity | Provider<AwsCredentialIdentity> | undefined> {
+    static async getAwsCredentials(
+        email: string, 
+        password: string, 
+        clientId: string = SCOUT_APP_CLIENT, 
+        idenityPoolId: string = COGNITO_IDENTITY_POOL, 
+        userPooldI: string = COGNITO_USER_POOL): Promise<AwsCredentialIdentity | Provider<AwsCredentialIdentity> | undefined> {
         
-        let credentials: AwsCredentialIdentity | Provider<AwsCredentialIdentity> | undefined
-
-        let output = await Cognito.authenticateUser(username.toLowerCase(), password)
+        let output = await Cognito.authenticateUser(email.toLowerCase(), password, clientId)
  
         if(output == undefined) {
-            credentials = undefined
             console.log("ERROR: User authentication returned undefined")
             return
         }
         let token = output.AuthenticationResult?.IdToken
         if(token == undefined) {
             console.log("ERROR: ID Token not found on user authentication output")
-            credentials = undefined
             return
         }
         console.log(`Found User ${username} ID Token`)
         try {
             const cognitoCredentials: CognitoIdentityCredentialProvider = fromCognitoIdentityPool({
                 clientConfig: { region: REGION }, // Configure the underlying CognitoIdentityClient.
-                identityPoolId: COGNITO_IDENTITY_POOL,
+                identityPoolId: idenityPoolId,
                 logins: {
-                    [COGNITO_IDP_TEMPLATE(REGION, COGNITO_USER_POOL)]: token,                            
+                    [COGNITO_IDP_TEMPLATE(REGION, userPooldI)]: token,                            
                 }            
             })
             console.log("Found AWS Credentials")
             return cognitoCredentials
-            
-            return
         } catch (err) {
             console.log("ERROR: Failed to retreive AWS credentials", err)
             return
         }                
     }
 
-
-    static async authenticateUser(username: string, password: string): Promise<InitiateAuthCommandOutput | undefined> {
+    static async authenticateUser(username: string, password: string, clientId: string = GLADIADORES_APP_CLIENT_ID): Promise<InitiateAuthCommandOutput | undefined> {
         const initiateAuthCommandInput: InitiateAuthCommandInput = {
             AuthFlow: 'USER_PASSWORD_AUTH',
-            ClientId: SCOUT_APP_CLIENT,
+            ClientId: clientId,
             AuthParameters: {
                 "USERNAME": username, 
                 "PASSWORD": password
@@ -66,8 +87,87 @@ export class Cognito {
             console.log(error)
             return undefined
         }
-
     }
 
-    
+    static async signUpUser(name: string, email: string, phoneNumber: string, password: string, role: string): Promise<SignUpCommandOutput  | undefined> {
+        const signUpCommandInput: SignUpCommandInput = {
+            ClientId: GLADIADORES_APP_CLIENT_ID,
+            Password: password,
+            Username: email,
+            UserAttributes: [
+                {
+                    Name: 'name',
+                    Value: name
+                },
+                {
+                    Name: 'email',
+                    Value: email
+                },
+                {
+                    Name: 'phone_number',
+                    Value: phoneNumber
+                },
+                {
+                    Name: 'custom:role',
+                    Value: role
+                }     
+            ],
+        }
+
+        try {
+            const command = new SignUpCommand(signUpCommandInput);
+            console.log("command: ", command)
+            let response = await client.send(command);
+            return response
+          } catch (error) {
+            console.log(error)
+            return undefined
+        }
+    }
+
+    static async confirmSignUpUser(code: string, email: string): Promise<ConfirmSignUpCommandOutput  | undefined> {
+        const confirmSignUpCommandInput: ConfirmSignUpCommandInput = {
+            ClientId: GLADIADORES_APP_CLIENT_ID,
+            ConfirmationCode: code,
+            Username: email,
+        }
+
+        try {
+            const command = new ConfirmSignUpCommand(confirmSignUpCommandInput);
+            console.log("command: ", command)
+            let response = await client.send(command);
+            return response
+          } catch (error) {
+            console.log(error)
+            return undefined
+        }
+    }
+
+    static async resendConfirmationCode(email: string): Promise<ResendConfirmationCodeCommandOutput  | undefined> {
+        const resendConfirmationCodeCommandInput: ResendConfirmationCodeCommandInput = {
+            ClientId: GLADIADORES_APP_CLIENT_ID,
+            Username: email,
+        }
+
+        try {
+            const command = new ResendConfirmationCodeCommand(resendConfirmationCodeCommandInput);
+            console.log("command: ", command)
+            let response = await client.send(command);
+            return response
+          } catch (error) {
+            console.log(error)
+            return undefined
+        }        
+    }
+
+    static getUserFromToken(idToken: string): User {
+        let jwt = jwtDecode(idToken) as {[key:string]: string}
+        return {
+            id: jwt['sub'],
+            name: jwt['name'],
+            phone: jwt['phone_number'],
+            email: jwt['email'],
+            role: jwt['custom:role'],
+        }
+    }
 }
--- angular-app/src/app/aws-clients/constants.ts
+++ angular-app/src/app/aws-clients/constants.ts
@@ -5,10 +5,14 @@ export const COGNITO_IDENTITY_POOL = 'us-east-1:66bd9702-735d-4e68-a155-76fb7fb2
 export const COGNITO_UNAUTHENTICATED_IDENTITY_POOL = 'us-east-1:adf17da9-3e55-4f06-b8fa-53554f6f4dbf'
 export const COGNITO_USER_POOL = 'us-east-1_sQjlRFiTn'
 export const SCOUT_APP_CLIENT = '7hbaqk501t9nd2sqjnl5ocs5e'
+
+export const COGNITO_AUTHENTICATED_IDENTITY_POOL = 'us-east-1:0c736515-3362-4b12-8d19-ac3f85f8d977'
+export const COGNITO_AUTHENTICATED_USER_POOL = 'us-east-1_ow53ANKbx'
+export const GLADIADORES_APP_CLIENT_ID = '5gg17rf05cav1h6qnnvnpeafiv'
+
 export const COGNITO_IDP_TEMPLATE = (region: string, userPoolId: string) => `cognito-idp.${region}.amazonaws.com/${userPoolId}`
 export const DDB_TABLE_NAME = 'Gladiadores'
 export const COGNITO_UNAUTHENTICATED_CREDENTIALS = fromCognitoIdentityPool({
     clientConfig: { region: REGION }, // Configure the underlying CognitoIdentityClient.
     identityPoolId: COGNITO_UNAUTHENTICATED_IDENTITY_POOL
 })
-
