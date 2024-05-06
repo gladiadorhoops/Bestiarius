a98fefb877dc663dac986ee403d20cc1839cd7c8
Daniele Braga Pecanha <daniele.pecanha@gmail.com>
2024-05-05 17:20:05 -0700

    Update color scheme and add signup form

--- angular-app/src/app/signup/signup.component.ts
+++ angular-app/src/app/signup/signup.component.ts
@@ -9,16 +9,18 @@ import { switchMap } from 'rxjs';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent {
-  form:FormGroup;
+  signupForm:FormGroup;
 
   constructor(private fb:FormBuilder, private activatedRoute: ActivatedRoute) {
     this.role = "";
-    this.form = this.fb.group({
+    this.email = "emailPlaceholder";
+    this.signupForm = this.fb.group({
       nombre: ['',Validators.required],
       email: ['',Validators.required],
       phone: ['',Validators.required],
-      password: ['',Validators.required]
-  });
+      password: ['',Validators.required],
+      codigo: {value: '', disabled: true}
+    });
   }
 
   ngOnInit() {
@@ -30,12 +32,47 @@ export class SignupComponent {
   }
 
   signup() {
-    console.log("Registering "+this.role+": "+this.form.value.nombre)
-    console.log("NEED TO IMPLEMENT SIGNUP FUNCTION")
+    console.log("Registering "+this.role+": "+this.signupForm.value.nombre);
+    console.log("Email: "+this.signupForm.value.email);
+    this.email = this.signupForm.value.email;
+    console.log("NEED TO IMPLEMENT SIGNUP FUNCTION");
+    this.confirmation = true;
+    this.signupForm.get('nombre')?.disable();
+    this.signupForm.get('email')?.disable();
+    this.signupForm.get('phone')?.disable();
+    this.signupForm.get('password')?.disable();
+    this.signupForm.get('codigo')?.enable();
+    this.popUpnMsg = "Verifica tu correo introduciendo tu código de verificación.  No olvides buscar en la carpeta de correo no deseado";
+    this.openPopup();
+  }
+
+  confirm() {
+    console.log("Validating "+this.role+": "+this.signupForm.value.email);
+    console.log("NEED TO IMPLEMENT CONFIRM FUNCTION");
+    this.confirmation = false;
+    this.signupForm.get('nombre')?.enable();
+    this.signupForm.get('email')?.enable();
+    this.signupForm.get('phone')?.enable();
+    this.signupForm.get('password')?.enable();
+    this.signupForm.get('codigo')?.disable();
+    this.signupForm.reset();
+    this.popUpnMsg = "Registro Completo!";
+    this.openPopup();
   }
 
 
+  openPopup() {
+    this.displayStyle = "block";
+  }
+  closePopup() {
+    this.displayStyle = "none";
+  }
+
   role: string | null;
+  email: string;
   failed: boolean = false;
+  confirmation: boolean = false;
+  displayStyle = "none";
+  popUpnMsg = ""
 
 }
