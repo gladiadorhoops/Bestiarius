08a057947fd720030a83f7b2920944fb8ca99f11
gladiadorhoops <103904958+gladiadorhoops@users.noreply.github.com>
2025-07-13 01:52:24 -0700

    feat: Add S3 file upload/download
    * Added S3 functions
    * Initialized S3 client in restricted area component so other components can use it

--- angular-app/src/app/aws-clients/s3.ts
+++ angular-app/src/app/aws-clients/s3.ts
@@ -1,4 +1,4 @@
-import { S3Client, GetObjectCommand, ListObjectsCommand, ListObjectsCommandInput, GetObjectCommandInput } from "@aws-sdk/client-s3";
+import { S3Client, GetObjectCommand, ListObjectsCommand, ListObjectsCommandInput, GetObjectCommandInput, PutObjectCommand, PutObjectCommandInput } from "@aws-sdk/client-s3";
 import { REGION, COGNITO_UNAUTHENTICATED_CREDENTIALS, TOURNAMENT_YEAR } from "./constants";
 import { AwsCredentialIdentity, Provider } from "@aws-sdk/types"
 
@@ -13,6 +13,7 @@ const CURRENT_TOURNAMENT = "10"
 const MATCH_DATA_PATH = `${MATCH_DATA}/${TOURNAMENT_PREFIX}${CURRENT_TOURNAMENT}`
 const REPORT_DATA = "reports"
 const REPORT_PATH = `${REPORT_DATA}/${TOURNAMENT_PREFIX}${TOURNAMENT_YEAR}`
+const IMAGE_PATH = "images"
 
 
 export enum ReportType {
@@ -61,6 +62,49 @@ export class S3 {
             return
         }
 
+    }
+
+    async uploadFile(fileName: string, fileContent: string | Uint8Array | Buffer, contentType: string): Promise<boolean> {
+        console.log(`Uploading file: ${fileName}`)
+        
+        const input: PutObjectCommandInput = {
+            Bucket: GLADIADORES_BUCKET_NAME,
+            Key: `${IMAGE_PATH}/${fileName}`,
+            Body: fileContent,
+            ContentType: contentType
+        };
+
+        console.log('Upload input:', input);
+        
+        try {
+            const response = await this.client.send(new PutObjectCommand(input));
+            console.log('Successfully uploaded file:', response);
+            return true;
+        } catch (err) {
+            console.error('Error uploading file:', err);
+            return false;
+        }
+    }
+
+    async downloadFile(fileName: string): Promise<Uint8Array | undefined> {
+        console.log(`Downloading file: ${fileName}`)
+        
+        const input: GetObjectCommandInput = {
+            Bucket: GLADIADORES_BUCKET_NAME,
+            Key: `${IMAGE_PATH}/${fileName}`
+        };
+
+        console.log('Download input:', input);
+        
+        try {
+            const response = await this.client.send(new GetObjectCommand(input));
+            const fileContent = await response.Body?.transformToByteArray();
+            console.log('Successfully downloaded file:', fileName);
+            return fileContent;
+        } catch (err) {
+            console.error('Error downloading file:', err);
+            return undefined;
+        }
     }
 
     static async build(credentials: AwsCredentialIdentity | Provider<AwsCredentialIdentity>): Promise<S3> {
