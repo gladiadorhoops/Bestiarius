41e13c9962dc6b6e42f5e9a3362220819c597be1
Jesus Gardea <jgardea35@outlook.com>
2025-07-26 04:52:10 -0700

    filtering team list when category is selected
    A team can only exist in one category (teams with same name in different categories are different resources).
    Hence, removing teams not in the category to avoid repeated names and team selection from a different category.
    Picking a team not in the category results on no matches degrading CX

--- angular-app/src/app/utils/utils.ts
+++ angular-app/src/app/utils/utils.ts
@@ -1,6 +1,22 @@
 import {v4 as uuid} from 'uuid';
+import { Match } from '../interfaces/match';
 
 export function generateId(): string {
     return uuid()
 }
- 
+
+export function filterMatches(
+    matches: Match[], 
+     day: string | null = null,
+     gym: string | null = null, 
+     team: string | null = null,
+    ): Match[] {
+    let filteredMatches: Match[] = matches
+    if(!matches) return filteredMatches;
+    if(day) filteredMatches = filteredMatches.filter(match => match.day == day);
+    if(gym) filteredMatches = filteredMatches.filter(match => match.location.id == gym);
+    if(team) filteredMatches = filteredMatches.filter(match => 
+      match.homeTeam.name == team || match.visitorTeam.name == team
+    );
+    return filteredMatches;
+}
